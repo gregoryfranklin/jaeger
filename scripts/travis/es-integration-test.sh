d2e0b3f6a0112f33298f4ca5a97959e46f83e19c
Greg Franklin <gregoryfranklin@rentalcars.com>
2019-07-19 11:40:14 +0100

    Make it possible to run es-integration-test against different versions of elasticsearch
    A convenient way to run the elasticsearch integration test is to just run ./scripts/travis/es-integration-test.sh
    
    This change allows you to specify different elasticsearch versions
    
    eg
    
    ES_VERSION=5.6.12 scripts/travis/es-integration-test.sh
    ES_VERSION=6.8.1 scripts/travis/es-integration-test.sh
    ES_VERSION=7.2.0 scripts/travis/es-integration-test.sh
    
    The default version to use for the tests is currently 5.6.12
    
    Signed-off-by: Greg Franklin <gregoryfranklin@rentalcars.com>

--- scripts/travis/es-integration-test.sh
+++ scripts/travis/es-integration-test.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-docker pull docker.elastic.co/elasticsearch/elasticsearch:5.6.12
-CID=$(docker run -d -p 9200:9200 -e "http.host=0.0.0.0" -e "transport.host=127.0.0.1" docker.elastic.co/elasticsearch/elasticsearch:5.6.12)
+# Default to elasticsearch 5.6.12 if ES_VERSION is not set
+ES_VERSION=${ES_VERSION:-5.6.12}
+
+docker pull docker.elastic.co/elasticsearch/elasticsearch:${ES_VERSION}
+CID=$(docker run -d -p 9200:9200 -e "http.host=0.0.0.0" -e "transport.host=127.0.0.1" docker.elastic.co/elasticsearch/elasticsearch:${ES_VERSION})
 export STORAGE=elasticsearch
 make storage-integration-test
 docker kill $CID
